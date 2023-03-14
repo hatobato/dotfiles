4ab4bb6f3df6c9dabceae26c2c37f3cfaafd2111
hatobato <hatobato61@gmail.com>
2023-03-14 12:06:59 +0900

    update

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+    [[ "$dotfile" == "${SCRIPT_DIR}/etc" ]] && continue
     ln -fnsv "$dotfile" "$HOME"
 done
